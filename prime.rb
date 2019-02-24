6e369d7f7c8fc542d7b811c45b290f13afa5d075
Daniel Ko <dankomong@gmail.com>
2019-02-24 22:10:52 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(int)
+  new_range = 2..(int - 1)
+  if int < 2
+    return false
+  elsif int == 2 
+    return true
+  end
+  for num in new_range do
+    if int % num == 0
+      return false
+    end
+  end
+  true
+end
