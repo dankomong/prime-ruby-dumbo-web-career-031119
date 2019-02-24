3b20a511509d94371a5ec46137fa25523cda313e
Daniel Ko <dankomong@gmail.com>
2019-02-24 22:09:39 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(int)
+  new_range = 2..int
+  if int < 2
+    false
+  elsif int == 2 
+    true
+  end
+  for num in new_range do
+    if int % num == 0
+      false
+    else 
+      true
+    end
+  end
+end
